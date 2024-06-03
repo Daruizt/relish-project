0d219c954cb3796dd1c9b4417723f52a4360f6ee
Daruizt <druiz@distelsa.com.gt>
2024-06-03 10:17:36 -0600

    Deploy and CI/CD correction 8

---
+++ dist/main.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const dto_validation_pipe_1 = require("./pipes/dto-validation.pipe");
+const error_handling_1 = require("./exception/configuration/error-handling");
+const errors_1 = require("./errors");
+const swagger_1 = require("@nestjs/swagger");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({ origin: true });
+    app.useGlobalPipes(new dto_validation_pipe_1.DtoValidationPipe());
+    app.useGlobalFilters(new error_handling_1.HttpExceptionFilter(errors_1.errorAppRepository));
+    const options = new swagger_1.DocumentBuilder()
+        .setTitle('Relish')
+        .setDescription('The Relish API doc')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, options);
+    swagger_1.SwaggerModule.setup('explorer', app, document);
+    await app.listen(process.env.PORT || 8080);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
